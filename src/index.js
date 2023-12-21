e42f52168094d2b3e98113b50732216b56f1d315
samyard <98907899+samyard@users.noreply.github.com>
2023-12-21 12:36:31 -0500

    src files update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
