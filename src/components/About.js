e817527b033a074277b5e70625c97bd76132d67c
samyard <98907899+samyard@users.noreply.github.com>
2023-12-21 12:36:54 -0500

    components update

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,66 @@
+import React, { useState, useEffect } from "react";
 
-import React from "react";
+const About = () => {
+  const [scrollY, setScrollY] = useState(0);
 
-export default function About() {
-    return (
-      <section id="about">
-        <div className="container mx-auto flex px-6 py-12 md:flex-row lg:px-15 flex-col items-center text-center">
-          <div className="lg:1/2 md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-center mb-16 md:mb-0 items-center text-center">
-          <p className="font-sans text-xl mb-4 text-white md:text-center">
-          Welcome to my Data Science portfolio. 
-             </p>
-             <p className="font-sans text-l mb-4 text-white text-center">
-          I specialize in extracting meaningful 
-          insights from data, blending analytics, and machine learning with a touch of
-           storytelling. As you explore my projects, you'll find a commitment to ethical
-            data modeling, ensuring that every analysis is not just insightful but responsible.
-             </p>
-             <div className="flex justify-center">
-              <a
-                href="#contact"
-                className="inline-flex text-white bg-blue-300 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 rounded text-lg transform transition-transform hover:scale-105">
-                Contact Me
-              </a>
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
 
-              <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg transform transition-transform hover:scale-105">
-                My Past Work
-              </a>
-            </div>
+    window.addEventListener("scroll", handleScroll);
 
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scale = 1 - scrollY * 0.0002;
+
+  return (
+    <section id="about" style={{ transform: `scale(${scale})` }}>
+      <div className="container mx-auto flex flex-col md:flex-row items-center text-center md:items-start md:text-left py-12 px-6 lg:px-15">
+        <div className="md:w-1/2 lg:pr-24 md:pr-16 mb-16 md:mb-0 flex flex-col justify-center">
+          <p className="font-sans text-3xl font-bold mb-4 text-white md:text-center">
+            Welcome to my Data Science portfolio.
+          </p>
+          <p className="font-sans text-l mb-4 text-white md:text-center">
+            I specialize in extracting meaningful insights from data, blending analytics, and machine learning with a touch of storytelling. As you explore my projects, you'll find a commitment to ethical data modeling, ensuring that every analysis is not just insightful but responsible.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#contact"
+              className="inline-flex text-center text-white bg-blue-300 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 rounded text-lg transform transition-transform hover:scale-105">
+              Contact Me
+            </a>
+
+            <a
+              href="#projects"
+              className="inline-flex text-center text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg transform transition-transform hover:scale-105">
+              My Past Work
+            </a>
           </div>
-          <div className="sm: w-1/2 lg:w-1/2 md:w-1/2 ">
+          {/* paragraph below buttons */}
+          <p className="font-sans text-l mt-4 text-white text-center">
+            My current projects include web design and version control, exploring new libraries to enhance the user experience.
+          </p>
+
+          <p className="font-sans text-l mt-1 text-white text-center">
+            My current interests lie in AI and data ethics, emphasizing responsible and transparent practices. I am particularly interested in contributing to discussions on ethical AI and ensuring its safe development and deployment.
+          </p>
+        </div>
+        <div className="md:w-1/2 flex items-center justify-center">
+          <div className="max-w-sm mx-auto">
             <img
-              className="object-cover object-center rounded w-1/2"
+              className="object-cover object-center rounded-full w-full h-full"
               alt=""
-              src="./self.png"
+              src="./linkedin.jpg"
             />
           </div>
         </div>
-      </section>
-    );
-  }
-  
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,55 @@
-
 // heroicons to write some SVG icons as react components
 import { TerminalIcon } from "@heroicons/react/solid"; 
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { projects } from "../data";
 
-export default function Projects() {
+const Projects = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const calculateScale = () => {
+    if (sectionRef.current) {
+      const sectionRect = sectionRef.current.getBoundingClientRect();
+      const sectionTop = sectionRect.top + window.scrollY;
+      const sectionBottom = sectionRect.bottom + window.scrollY;
+
+      // Adjust the scale calculation based on your requirements
+      const scaleFactor = 0.0003;
+      const scaleIn = Math.min(1 + (scrollY - sectionTop) * scaleFactor, 1);
+      const scaleOut = Math.min(1 + (sectionBottom - scrollY) * scaleFactor, 1);
+
+      // Choose the appropriate scaling direction based on the scroll position
+      return scrollY >= sectionTop && scrollY <= sectionBottom ? scaleIn : scaleOut;
+    }
+
+    // Default scale if sectionRef is not available
+    return 1;
+  };
+
+  const scale = calculateScale();
+
+
+
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font"  ref={sectionRef}
+    >
+      <div className="container px-10 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <TerminalIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white"
+          style={{ transform: `scale(${scale})` }}>
             Projects
           </h1>
         </div>
@@ -19,14 +58,17 @@ export default function Projects() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-full md:w-1/2 lg:w-1/2 p-2 md:p-4">
+              className="sm:w-1/2 md:1/2 lg:w-1/3 p-2"
+            >
               <div className="relative h-60">
                 <img
                   alt=""
-          className={`object-cover w-full h-full ${project.isRounded ? 'rounded-lg' : 'rounded'}`}
+                  className={`object-cover w-full h-full ${
+                    project.isRounded ? "rounded-lg" : "rounded"
+                  }`}
                   src={project.image}
                 />
-                  <div className="absolute inset-0 px-4 py-6 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-90">
+                <div className="absolute inset-0 px-4 py-6 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-90">
                   <h2 className="tracking-widest text-sm title-font font-medium text-blue-300 mb-1">
                     {project.subtitle}
                   </h2>
@@ -42,4 +84,6 @@ export default function Projects() {
       </div>
     </section>
   );
-}
+};
+
+export default Projects;
